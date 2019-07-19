4fba2f2b008e7ce0958472bbe154bcc89c2d0832
Michelle <apple@Apples-MacBook-Pro-98.local>
2019-07-19 09:49:43 -0700

    Initial Commit

---
+++ Contents.swift
@@ -0,0 +1,84 @@
+import UIKit
+
+var num1 = 1
+var num2 = 2
+var num3 = 3
+
+//true of false
+//this prints true
+//print(num1<num2)
+//this prints false
+//print(num1>=num2)
+
+//prints false
+//print(num1==num2)
+
+//checking if num1 is not equal to num2
+//this prints true
+//print(num1 != num2)
+
+//check 1+2 == 3
+//if num1 + num2 == num3 && num2 + num1 == num3 {
+//    //then do something
+//    print("yes!")
+//}
+
+//checking if 1 equals 2, since the second part is true, now "yes!" prints 
+//if num1 == num2 || num1 == num1 {
+//    print("yes!")
+//}
+
+//print(5<3) //false
+//print(12>7) //true
+//print("Karlie" == "karlie") //false
+//print(luckyNum == 7) //error
+//print(7 == "7") //error
+//print(6 != 8) //true
+//"karlie" == "karlie" //true
+
+//var dogAge = 1
+//
+////if this is true
+//if dogAge > 10 {
+//    //then I do this
+//    print("Old Doggie")
+//} else if dogAge == 10 { //exactly
+//    //then I do this instead
+//    print("Doggie")
+//} else { //then I do this instead
+//    print("Young puppy")
+//}
+
+//if dogAge < 2 {
+//    print("You are a puppy 🐶 ")
+//}
+//
+//if dogAge == 2 {
+//    print("You are awesome!")
+//}
+//
+//if dogAge == 3 {
+//    print("You are awesome!")
+//}
+//
+//if dogAge == 4 {
+//    print("You are awesome!")
+//}
+//
+//// etc.
+//
+//if dogAge > 12 {
+//    print("You are elderly 💗 " )
+//}
+
+var favoriteFood == "Peach Tea"
+
+if "Chipotle" == favoriteFood {
+    print("My favorite foooddd!")
+} else if "Starbucks" == favoriteFood {
+    print("You like Starbucks.")
+} else if "Raisins" == favoriteFood {
+    print("tomatoes I guess")
+} else {
+    print("I suck at guessing ☹️")
+}
